202bb3ca1fbdfff27c40de9ae0ae1f56ed4b190f
baekinjun <qor7111@naver.com>
2021-08-31 21:16:32 +0900

    quiz kafka

--- src/main/java/com/injun/quiz/controller/AttemptController.java
+++ src/main/java/com/injun/quiz/controller/AttemptController.java
@@ -1,4 +1,43 @@
 package com.injun.quiz.controller;
 
-public class AttemptController {
+import com.injun.quiz.domain.Attempt;
+import com.injun.quiz.service.QuizService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/results")
+public class AttemptControllr {
+    private final QuizService quizService;
+
+    @RequiredArgsConstructor
+    @NoArgsConstructor(force = true)
+    @Getter
+    static final class ResultResponse {
+        private final boolean correct;
+    }
+
+    @PostMapping
+    ResponseEntity<Attempt> postResult(@RequestBody Attempt attempt) {
+        boolean isCorrect = quizService.checkAttempt(attempt);
+        //교차 엔티티라고 생각하면 된다.
+        Attempt attemptCopy = new Attempt(attempt.getUser(), attempt.getQuiz(), attempt.getResultAttempt(), isCorrect);
+        return ResponseEntity.ok(attemptCopy);
+    }
+
+    @GetMapping
+    ResponseEntity<Flux<Attempt>> getStatics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(quizService.getStaticsForUser(alias));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Mono<Attempt>> getResultById(final @PathVariable("id") long id) {
+        return ResponseEntity.ok(quizService.getResultById(id));
+    }
 }
--- src/main/java/com/injun/quiz/service/QuizService.java
+++ src/main/java/com/injun/quiz/service/QuizService.java
@@ -1,9 +1,17 @@
 package com.injun.quiz.service;
 
 
+import com.injun.quiz.domain.Attempt;
 import com.injun.quiz.domain.Quiz;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface QuizService {
     Mono<Quiz> createQuiz();
+
+    boolean checkAttempt(Attempt attempt);
+
+    Flux<Attempt> getStaticsForUser(String alias);
+
+    Mono<Attempt> getResultById(long id);
 }
